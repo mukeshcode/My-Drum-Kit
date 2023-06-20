551cc15e21544392d41603794a4f12a017bc9186
Mukesh Kumar <mukesh.official14@gmail.com>
2023-06-20 19:55:30 +0530

    Added files via upload.

---
+++ index.js
@@ -0,0 +1,62 @@
+var elem = document.getElementsByClassName("btn");
+    for (var i = 0; i < elem.length; i++) {
+      elem[i].addEventListener("click", function(){
+        playSound(this.textContent);
+        playAnimation(this.textContent);
+      });
+    }
+
+    document.addEventListener("keydown", function(event){
+      // console.log(event);
+      playSound(event.key);
+      playAnimation(event.key);
+    });
+    
+    function playSound(letter){
+      switch(letter){
+        case "w" : 
+          var crash = new Audio("./sounds/crash.mp3");
+          crash.play();
+          break;
+
+        case "a" : 
+          var kickBass = new Audio("./sounds/kick-bass.mp3");
+          kickBass.play();
+          break;
+
+        case "s" :
+          var snare = new Audio("./sounds/snare.mp3");
+          snare.play();
+          break;
+
+        case "d" : 
+          var tom_1 = new Audio("./sounds/tom-1.mp3");
+          tom_1.play();
+          break;
+
+        case "j" : 
+          var tom_2 = new Audio("./sounds/tom-2.mp3");
+          tom_2.play();
+          break;
+
+        case "k" : 
+          var tom_3 = new Audio("./sounds/tom-3.mp3");
+          tom_3.play();
+          break;
+
+        case "l" : 
+          var tom_4 = new Audio("./sounds/tom-4.mp3");
+          tom_4.play();
+
+        default :   
+          console.log("Unknown event !");
+      }
+    }
+
+    function playAnimation(currentKey){
+      var key = document.querySelector("." + currentKey);
+      key.classList.add("pressed");
+      setTimeout(() => {
+        key.classList.remove("pressed");
+      }, 100);
+    }
